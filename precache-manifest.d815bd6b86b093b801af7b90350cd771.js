ebdbddaf598dcddbc296b8f099c4c9ce34cf3b87
pen <penouc@gmail.com>
2018-10-18 11:53:16 +0800

    Updates

---
+++ precache-manifest.d815bd6b86b093b801af7b90350cd771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e86284a6eb8e2a806f7",
+    "url": "/gosmter/static/js/runtime~main.0e86284a.js"
+  },
+  {
+    "revision": "94588d4cc5050ad0cff9",
+    "url": "/gosmter/static/js/main.94588d4c.chunk.js"
+  },
+  {
+    "revision": "be7a60320b75f96d28a8",
+    "url": "/gosmter/static/js/1.be7a6032.chunk.js"
+  },
+  {
+    "revision": "94588d4cc5050ad0cff9",
+    "url": "/gosmter/static/css/main.d6fe96fd.chunk.css"
+  },
+  {
+    "revision": "4cae612b4d47f4822ef8ad7a64fdb51d",
+    "url": "/gosmter/index.html"
+  }
+];
